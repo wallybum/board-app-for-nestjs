52f4258282bb5f96b11c7c611b429f4cceff7bdc
wallybum <elvvis93@gmail.com>
2024-01-14 17:02:08 +0900

    Pipe를 이용한 유효성 체크 - class-validator, class-transformer 모듈 설치 - 파라미터 레벨의 파이프 적용

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,8 +1,12 @@
+import { IsNotEmpty } from "class-validator";
+
 /*
- DTO는 Class와 Interface 둘 다 사용 가능하나, 
- NextJS는 Class 로 만들 것을 권고.
+ DTO는 Class와 Interface 둘 다 사용 가능하나, NextJS는 Class 로 만들 것을 권고.
 */
 export class CreateBoardDto {
+    @IsNotEmpty()
     title: string;
+    
+    @IsNotEmpty()
     description: string;
 }
