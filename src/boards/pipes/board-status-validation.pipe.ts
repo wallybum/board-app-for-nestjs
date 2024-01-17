12fcdfd53b7ac9f6582345fe8d64a46f073208cf
wallybum <elvvis93@gmail.com>
2024-01-17 22:05:38 +0900

    커스텀 파이프를 이용한 유효성 검증

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,28 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+export class BoardStatusValidationPipe implements PipeTransform {
+
+
+    readonly StatusOptions = [
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC
+    ]
+
+
+    // value는 처리가 된 인자의 값이며, metaData는 인자에 대한 메타 데이터를 포함한 객체.
+    transform(value: any, metadata: ArgumentMetadata) {
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} isn't in the status options`)
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1
+    }
+}
