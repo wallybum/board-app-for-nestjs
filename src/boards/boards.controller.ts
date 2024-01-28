3b113bb39c1248e063b525538ce9cc189dc2cc04
wallybum <elvvis93@gmail.com>
2024-01-28 21:59:02 +0900

    id를 이용하여 게시물 가져오기 기능을 로컬메모리에서 TypeORM으로 변경

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,41 +1,46 @@
 import { Controller, Get, Post, Body, Param, Delete, Patch, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board-entity';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) { }
 
-    @Get('/')
-    getAllBoard(): Board[] {
-        return this.boardService.getAllBoards();
-    }
+    // @Get('/')
+    // getAllBoard(): Board[] {
+    //     return this.boardService.getAllBoards();
+    // }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    createBoard(
-        @Body() createBoardDto: CreateBoardDto
-    ): Board {
-        return this.boardService.createBoard(createBoardDto)
-    }
+    // @Post()
+    // @UsePipes(ValidationPipe)
+    // createBoard(
+    //     @Body() createBoardDto: CreateBoardDto
+    // ): Board {
+    //     return this.boardService.createBoard(createBoardDto)
+    // }
 
     @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardService.getBoardById(id)
+    getBoardById(@Param('id') id:number) : Promise<Board>{
+        return this.boardService.getBoardById(id);
     }
+    // @Get('/:id')
+    // getBoardById(@Param('id') id: string): Board {
+    //     return this.boardService.getBoardById(id)
+    // }
 
-    @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
-        this.boardService.deleteBoard(id);
-    }
+    // @Delete('/:id')
+    // deleteBoard(@Param('id') id: string): void {
+    //     this.boardService.deleteBoard(id);
+    // }
 
-    @Patch('/:id/status')
-    updateBoardStatus(
-        @Param('id') id: string,
-        @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    ) {
-        return this.boardService.updateBoardStatus(id, status);
-    }
+    // @Patch('/:id/status')
+    // updateBoardStatus(
+    //     @Param('id') id: string,
+    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    // ) {
+    //     return this.boardService.updateBoardStatus(id, status);
+    // }
 }
